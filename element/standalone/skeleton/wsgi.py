77eb933c6bdd3ceea26a459d2c61cd516d199366
Thomas Rabaix <thomas.rabaix@gmail.com>
2013-05-19 23:11:55 +0200

    Fix wsgi script

--- element/standalone/skeleton/wsgi.py
+++ element/standalone/skeleton/wsgi.py
@@ -8,19 +8,18 @@ This script must by the wsgi handler to start the application.
 You can customize it for your need.
 """
 
-if __name__ == "__main__":
+debug = False
 
-    debug = False
-    
-    if debug:
-        logging.basicConfig(level=logging.DEBUG)
+if debug:
+    logging.basicConfig(level=logging.DEBUG)
 
-    parameters = {
-        'ioc.debug': debug,
-        'ioc.env': 'prod',
-        'project.root_folder': os.path.dirname(os.path.realpath(__file__))
-    }
-    
-    flask = get_container(parameters).get("ioc.extra.flask.app")
-    flask.run(debug=debug)
-    
+parameters = {
+    'ioc.debug': debug,
+    'ioc.env': 'prod',
+    'project.root_folder': os.path.dirname(os.path.realpath(__file__))
+}
+
+app = get_container(parameters).get("ioc.extra.flask.app")
+
+if __name__ == '__main__':
+    app.run(debug=debug)
