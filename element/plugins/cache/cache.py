624088bbeef9b0d739b92c7c001465c5f82a2f83
Thomas Rabaix <thomas.rabaix@gmail.com>
2014-03-14 12:12:46 -0400

    Continue code migration to Tornado

--- element/plugins/cache/cache.py
+++ element/plugins/cache/cache.py
@@ -1,24 +1,26 @@
-import re
-
 class CacheControl(object):
+    """
+    This class attach cache header to the request matching the pattern
+    """
+
     def __init__(self, rules=None):
         self.rules = rules or []
 
     def cache_control(self, event):
-        response = event.get('response')
-        request = event.get('request')
+        request_handler = event.get('request_handler')
         context = event.get('context')
 
-        if request.method in ["GET", "HEAD"]:
-            rule = self.find_rule(context.node)
+        if request_handler.request.method in ["GET", "HEAD"]:
+            rule = self.find_rule(request_handler.request.path)
         else:
             rule = self.get_default()
 
-        response.headers['Cache-Control'] = ", ".join(rule['Cache-Control'])
+        for name, value in rule.iteritems():
+            request_handler.set_header(name, ", ".join(value))
 
-    def find_rule(self, node):
+    def find_rule(self, path):
         for rule in self.rules:
-            if rule['path'].match(node.id):
+            if rule['path'].match(path):
                 return rule
 
         return self.get_default()
