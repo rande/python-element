e7a827a6bff80682d01cb26890d5d32bc6e9fb20
Thomas <thomas.rabaix@gmail.com>
2014-04-01 23:08:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="element",
-    version="0.0.1",
+    version="0.0.2",
     description="Element: a node based cms",
     author="Thomas Rabaix",
     author_email="thomas.rabaix@gmail.com",
